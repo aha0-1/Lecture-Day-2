83d8eb5484d79578661f1d26e5df501b9c7081e3
annha <annha@LAPTOP-JNJ97GS6>
2022-01-24 11:04:34 -0600

    Add project files.

---
+++ DAY 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAY_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MIS 3033");
+        }
+    }
+}
